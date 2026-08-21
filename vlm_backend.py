d3b9ad7dfdaf16bacfaeb723724585d385111722
nevlock59-langlock <274181406+nevlock59-langlock@users.noreply.github.com>
2026-08-21 17:17:25 +0900

    Add Korean text recognition model to PaddleOCR

--- vlm_backend.py
+++ vlm_backend.py
@@ -35,6 +35,7 @@ def _get_paddle_engine():
         _paddle_ocr_engine = PaddleOCR(
             lang="korean",
           text_detection_model_name="PP-OCRv5_mobile_det",
+          text_recognition_model_name="korean_PP-OCRv5_mobile_rec",
             use_doc_orientation_classify=False,
             use_doc_unwarping=False,
             use_textline_orientation=False,
