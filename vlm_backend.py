d93bb74269953fc0dcb13d56bd0d9cb8eff2a20d
nevlock59-langlock <274181406+nevlock59-langlock@users.noreply.github.com>
2026-09-13 19:49:12 +0900

    Enable document orientation classification in OCR

--- vlm_backend.py
+++ vlm_backend.py
@@ -36,7 +36,7 @@ def _get_paddle_engine():
             lang="korean",
           text_detection_model_name="PP-OCRv5_mobile_det",
           text_recognition_model_name="korean_PP-OCRv5_mobile_rec",
-            use_doc_orientation_classify=False,
+            use_doc_orientation_classify=True,
             use_doc_unwarping=False,
             use_textline_orientation=False,
         )
